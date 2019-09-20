5672ecff8b97814856a68eb2694d9448cb482aa8
zodiac-G12 <llosershouyou@gmail.com>
2019-09-20 18:07:28 +0900

    second

---
+++ layouts/default.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <nuxt />
+    </div>
+</template>
+
+
+
+<script>
+
+import Vue from 'vue';
+import { Divider } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';  // or 'ant-design-vue/dist/antd.less'
+Vue.use(Divider);
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { faAngular, faTwitterSquare, faGithubSquare, faLinux } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faUserSecret);
+library.add(faAngular);
+library.add(faTwitterSquare);
+library.add(faGithubSquare);
+library.add(faLinux);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+export default {}
+
+</script>
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=PT+Serif|Open+Sans|Bevan|Spectral+SC|Ubuntu+Mono|Black+Ops+One|Bungee+Inline|Bungee+Shade&display=swap');
+
+/* 縦長 */
+@media screen and (orientation: portrait) {
+    :root{
+        --font-size: 3.5vw;
+        --landscape: none;
+        --portrait: block;
+    }
+}
+/* 横長 */
+@media screen and (orientation: landscape) {
+    :root{
+        --font-size: 1.5vw;
+        --landscape: block;
+        --portrait: none;
+    }
+}
+
+
+html, body{
+    margin: 0;
+    padding: 0;
+    font-family: "PT Serif","Ubuntu Mono", monospace, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+    font-size: var(--font-size);
+}
+
+h1,h2,h3,h4{
+    margin: 0;
+    text-align: center;
+    color: black;
+}
+
+.title{
+    font-family: 'Bungee Shade';
+    color: white;
+}
+
+.black-head{
+    width: 100vw;
+    height: 20vh;
+    background: black;
+    margin: 0;
+    color: snow;
+}
+
+.icon{
+    font-size: calc(var(--font-size)*1.5);
+    width: calc(var(--font-size)*1.5);
+    height: calc(var(--font-size)*1.5);
+    transform: translateY(10%);
+}
+
+.foot{
+    width: 100vw;
+    height: 15vh;
+    background: black;
+    text-align: center;
+    padding-top: 3vh;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="black-head">
+            <h1 class="title" style="line-height:15vh;">ZODIAC BLOG</h1>
+            <h4 style="line-height:1vh;color:snow;">~ 技術的な防備録 ~</h4>
+        </div>
+        <main style="margin:2vw;color:black;height:55vh">
+        <h3 style="margin-top:2vh;">このブログはNuxt.jsを使っています。 - {{now}}</h3>
+            <b>
+                <font-awesome-icon class="icon" style="color:midnightblue;" :icon="['fab', 'linux']" /> 
+                    ＜なぜNuxtを選んだのか？
+            </b>
+            <a-divider style="background:black;" type="horizontal"/>
+                <p><font-awesome-icon class="icon" style="color:#fd7e14;" :icon="['fab', 'angular']" /> 
+                    ＜なんとなくですね。SEO的にどうなのかしらと。</p>
+        </main>
+        <footer class="foot">
+            <font-awesome-icon class="icon" style="color:gold;" :icon="['fab', 'twitter-square']" /> 
+            <font-awesome-icon class="icon" style="color:gold;" :icon="['fab', 'github-square']" /> 
+            <p style="color:snow;">&copy; ZODIAC-G12 {{now}}</p>
+        </footer>
+    </div>
+</template>
+
+
+<script>
+
+import moment from 'moment-timezone';
+
+export default{
+    data: function() {
+        return {
+            now: moment().tz("Asia/Tokyo").format("ll"),
+        }
+    }
+}
+
+</script>
+
+
+<style>
+</style>
