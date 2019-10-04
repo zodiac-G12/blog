a5c931eae139b74529e53e6b15fcf29f95553636
zodiac-G12 <12181838+zodiac-G12@users.noreply.github.com>
2019-10-04 17:22:07 +0900

    10月

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,12 +3,14 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _61a1fd78 = () => interopDefault(import('../pages/2019-09-20.vue' /* webpackChunkName: "pages/2019-09-20" */))
-const _6115126e = () => interopDefault(import('../pages/2019-09-25.vue' /* webpackChunkName: "pages/2019-09-25" */))
-const _60f8e36c = () => interopDefault(import('../pages/2019-09-26.vue' /* webpackChunkName: "pages/2019-09-26" */))
-const _60c08568 = () => interopDefault(import('../pages/2019-09-28.vue' /* webpackChunkName: "pages/2019-09-28" */))
-const _47ee84ab = () => interopDefault(import('../pages/config.vue' /* webpackChunkName: "pages/config" */))
-const _6d6da479 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _59133e47 = () => interopDefault(import('../pages/2019-09-20.vue' /* webpackChunkName: "pages/2019-09-20" */))
+const _5959b3cc = () => interopDefault(import('../pages/2019-09-25.vue' /* webpackChunkName: "pages/2019-09-25" */))
+const _5967cb4d = () => interopDefault(import('../pages/2019-09-26.vue' /* webpackChunkName: "pages/2019-09-26" */))
+const _5983fa4f = () => interopDefault(import('../pages/2019-09-28.vue' /* webpackChunkName: "pages/2019-09-28" */))
+const _432daeb6 = () => interopDefault(import('../pages/2019-10-03.vue' /* webpackChunkName: "pages/2019-10-03" */))
+const _433bc637 = () => interopDefault(import('../pages/2019-10-04.vue' /* webpackChunkName: "pages/2019-10-04" */))
+const _d3b089a4 = () => interopDefault(import('../pages/config.vue' /* webpackChunkName: "pages/config" */))
+const _ac7bdcd4 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
 
@@ -21,27 +23,35 @@ export const routerOptions = {
 
   routes: [{
       path: "/2019-09-20",
-      component: _61a1fd78,
+      component: _59133e47,
       name: "2019-09-20"
     }, {
       path: "/2019-09-25",
-      component: _6115126e,
+      component: _5959b3cc,
       name: "2019-09-25"
     }, {
       path: "/2019-09-26",
-      component: _60f8e36c,
+      component: _5967cb4d,
       name: "2019-09-26"
     }, {
       path: "/2019-09-28",
-      component: _60c08568,
+      component: _5983fa4f,
       name: "2019-09-28"
     }, {
+      path: "/2019-10-03",
+      component: _432daeb6,
+      name: "2019-10-03"
+    }, {
+      path: "/2019-10-04",
+      component: _433bc637,
+      name: "2019-10-04"
+    }, {
       path: "/config",
-      component: _47ee84ab,
+      component: _d3b089a4,
       name: "config"
     }, {
       path: "/",
-      component: _6d6da479,
+      component: _ac7bdcd4,
       name: "index"
     }],
 
