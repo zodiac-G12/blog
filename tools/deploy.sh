d60ae9d36631486e18d3ccb424619f9ac940d85c
zodiac-G12 <12181838+zodiac-G12@users.noreply.github.com>
2020-05-27 11:52:48 +0900

    hoge

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,1 +1,1 @@
-sudo npm prune && sudo git add --all && sudo git commit -m jidou && sudo git push && sudo npm run generate:master && sudo npm run deploy
+sudo npm prune && sudo npm run generate:master && sudo git add --all && sudo git commit -m jidou && sudo git push && sudo npm run deploy
