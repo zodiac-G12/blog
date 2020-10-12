8ff6cb8a673f36b930a85297db94eb0a1bad2ec3
zodiac-G12 <12181838+zodiac-G12@users.noreply.github.com>
2020-10-12 23:49:18 +0900

    hage

--- components/logo.vue
+++ components/logo.vue
@@ -1,67 +1,34 @@
 <template>
-    <div class="logo">
-        <div class="triangle2"></div>
-        <div class="triangle1"></div>
-        <div class="triangle3"></div>
-        <div class="triangle4"></div>
-    </div>
+  <svg class="NuxtLogo" width="245" height="180" viewBox="0 0 452 342" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fill-rule="evenodd">
+      <path
+        d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
+        fill="#00C58E"
+      />
+      <path
+        d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
+        fill="#108775"
+      />
+      <path
+        d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
+        fill="#2F495E"
+        fill-rule="nonzero"
+      />
+    </g>
+  </svg>
 </template>
-
 <style>
-.logo {
-    animation: turn 2s linear forwards 1s;     
-    transform: scale(0.5, 0.5) rotateX(180deg);
-    position: relative;
-    height: 346px;
-    width: 500px;
-    margin: 10px auto;
-    margin-top: var(--logo-top);
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
 }
 
-.triangle1, .triangle2, .triangle3, .triangle4 {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0;
-    height: 0;
-}
-.triangle1 {
-    border-left: 210px solid transparent;
-    border-right: 210px solid transparent;
-    border-bottom: 360px solid #41B883;
-}
-.triangle2 {
-    top: 60px;
-    left: 70px;
-    animation: goright 0.5s linear forwards 3.5s;
-    border-left: 175px solid transparent;
-    border-right: 175px solid transparent;
-    border-bottom: 300px solid #3B8070;
-}
-.triangle3 {
-    top: 120px;
-    left: 70px;
-    animation: goright 0.5s linear forwards 3.5s;
-    border-left: 140px solid transparent;
-    border-right: 140px solid transparent;
-    border-bottom: 240px solid #35495E;
-}
-.triangle4 {
-    top: 240px;
-    left: 140px;
-    animation: godown 0.5s linear forwards 3s;
-    border-left: 70px solid transparent;
-    border-right: 70px solid transparent;
-    border-bottom: 120px solid #fff;
-}
-
-@keyframes turn {
-    100% {transform: scale(0.5, 0.5) rotateX(0deg);}
-}
-@keyframes godown {
-    100% {top: 360px;}
-}
-@keyframes goright {
-    100% {left: 140px;}
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
