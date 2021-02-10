cb7693dfc35df9b511332aeb436a29c080280bb1
zodiac-G12 <12181838+zodiac-G12@users.noreply.github.com>
2021-02-10 14:40:44 +0900

    RunAutoDeployScript

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,6 +4,7 @@ import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
 const _0cd788bd = () => interopDefault(import('../pages/content1.vue' /* webpackChunkName: "pages/content1" */))
+const _e3d5ed7a = () => interopDefault(import('../pages/content10.vue' /* webpackChunkName: "pages/content10" */))
 const _0ce5a03e = () => interopDefault(import('../pages/content2.vue' /* webpackChunkName: "pages/content2" */))
 const _0cf3b7bf = () => interopDefault(import('../pages/content3.vue' /* webpackChunkName: "pages/content3" */))
 const _0d01cf40 = () => interopDefault(import('../pages/content4.vue' /* webpackChunkName: "pages/content4" */))
@@ -35,6 +36,10 @@ export const routerOptions = {
     path: "/content1",
     component: _0cd788bd,
     name: "content1"
+  }, {
+    path: "/content10",
+    component: _e3d5ed7a,
+    name: "content10"
   }, {
     path: "/content2",
     component: _0ce5a03e,
